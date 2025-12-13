b89179dbe7ea61cd1b25f00f85a63a0a1d97e77a
BP-36-201-19 <>
2025-12-13 14:15:02 +0300

    structure fix

---
+++ PolyFixit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PolyFixit
+//
+//  Created by BP-36-201-19 on 13/12/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
