98bccd8cf184f809dc782a4f90793d48406d8fae
Hussain Humaidan <101637725+hussain-humaidan@users.noreply.github.com>
2025-12-21 18:51:52 +0300

    Delete Signin-Signout-ForgotPasswd/Technician/SignIn directory

--- Signin-Signout-ForgotPasswd/Technician/SignIn/SignIn/main.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-
-_ = UIApplicationMain(
-    CommandLine.argc,
-    CommandLine.unsafeArgv,
-    NSStringFromClass(TechnicianApplication.self),
-    NSStringFromClass(AppDelegate.self)
-)
